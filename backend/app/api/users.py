fd7ae91268ff98d9d60b7cda4b7232db59ff606a
Khushi Singh <khushii0304singh@gmail.com>
2026-08-09 16:00:05 +0530

    Update users.py

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 
+
+# Create an API router
+# prefix="/users" means all routes will start with /users
+# tags=["Users"] groups this API under Users in Swagger
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
-
+# Create a GET API endpoint
+# Final endpoint: GET /users/
 @router.get("/")
 def get_users():
     return {"message": "Users API Working"}
